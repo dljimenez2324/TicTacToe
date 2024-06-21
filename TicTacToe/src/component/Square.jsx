989f74eecf4ae3b74e888b235e43c8b08e7c10d9
dljimenez2324 <dljimenez2324@gmail.com>
2024-06-20 22:49:07 -0700

    completed the steps for "Lifting state up"  in the tutorial at    https://react.dev/learn/tutorial-tic-tac-toe#setup-for-the-tutorial

--- TicTacToe/src/component/Square.jsx
+++ TicTacToe/src/component/Square.jsx
@@ -1,18 +1,20 @@
 // import react from 'react';
 import { useState } from 'react';
 
-const Square = () => {
+const Square = ({value, onSquareClick}) => {
 
-    const [value, setValue] = useState(null);
+    // const [value, setValue] = useState(null);
 
     // handleClick function to show that the button was clicked    This function is fet into the onClick function in the button
-    function handleClick() {
-        return setValue('X');
-    }
+    // function handleClick() {
+    //     return setValue('X');
+    // }
 
     return (
         <>
-            <button className="square" onClick={handleClick}>{value}</button>
+            <button className="square" onClick={onSquareClick}>
+                {value}
+            </button>
         </>
     )
 }
